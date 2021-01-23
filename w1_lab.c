36e7390ca8312063c886ec2006b1e12cba36f97e
Yejin Kim <55108799+yejinkim97@users.noreply.github.com>
2021-01-23 00:46:31 -0500

    Add files via upload

---
+++ w1_lab.c
@@ -0,0 +1,15 @@
+/* 
+	Name: Ye Jin Kim
+	Email: yjkim33@myseneca.ca
+	Student Number: 163291180
+
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("*** Welcome to C Programming ***");
+	return 0;
+}
